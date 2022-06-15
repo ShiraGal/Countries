c3566b2834f13b409c2bb8413106d75fb8b34d1d
shira <shira8sara@gmail.com>
2022-06-15 11:07:32 +0300

    works

--- src/component/OpenCountry.jsx
+++ src/component/OpenCountry.jsx
@@ -9,14 +9,16 @@ export default function OpenCountry(props) {
   const { countries, setCountries } = useContext(CountriesContext);
 
   return (
-    <div className="openCountry">
-      <h1>{choice.name.common}</h1>
-      <img src={choice.flags.png} className="openFlag"></img>
-      <h2>Population: </h2>
-      <h2 className="greyValue">{choice.population}</h2>
-      <h2>Languages: </h2>
-      <h2 className="greyValue">{Object.values(choice.languages)}</h2>
-      <button onClick={() => setPop("close")}>close</button>
+    <div className="backgroundPop">
+      <div className="openCountry">
+        <h1>{choice.name.common}</h1>
+        <img src={choice.flags.png} className="openFlag"></img>
+        <h2>Population: </h2>
+        <h2 className="greyValue">{choice.population}</h2>
+        <h2>Languages: </h2>
+        <h2 className="greyValue">{Object.values(choice.languages)}</h2>
+        <button onClick={() => setPop("close")}>close</button>
+      </div>
     </div>
   );
 }
